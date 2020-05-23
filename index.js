3978858243f8c9111bb33df14ea5b7c660b798c9
patrickfinnigan <patrickfinnigan94@gmail.com>
2020-05-22 18:15:59 -0700

    getting CRUDdy with it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
 
 const app = express();
@@ -7,9 +8,20 @@ const app = express();
 //Init middleware
 // app.use(logger);
 
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 // Body Parser middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
+
+// Homepage route
+app.get('/', (req, res) => res.render('index'));
 
 //Set a static folder
 app.use(express.static(path.join(__dirname, "public")));
